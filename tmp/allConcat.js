be20d03d8e053c6a2001b91b7a7ffb6e33f1dfbe
Nhat Hoang <hoangnh092185@gmail.com>
2016-10-14 09:46:27 -0700

    create a response to append username to the front end.

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -4,12 +4,13 @@ var apiKey = require('./../.env').apiKey;
 $(document).ready(function(){
   $('#find-username').click(function(event){
     event.preventDefault();
+    $('#showUserName').append(" ");
     console.log("Hi, i'm an object.");
     var userName = $('#userName').val();
     $('#userName').val("");
     $.get('https://api.github.com/users/'+userName+'?access_token=' + apiKey).then(function(response){
-     console.log(response);
-     $('#showUSerName').append("User Name :"+ response.name())
+     console.log(response.name);
+     $('#showUserName').append("User Name : "+ response.name);
    }).fail(function(error){
      console.log(error.responseJSON.message);
    });
